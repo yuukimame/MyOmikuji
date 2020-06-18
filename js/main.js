9e724ba1ef7ee4e45d6c69a135ec0ef6a1f540dd
MKoyama <yuuki.mrm@gmail.com>
2020-06-18 10:24:52 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+{
+	const btn = document.getElementById('btn');
+
+	btn.addEventListener('click', () => {
+		// const results = ['大吉','中吉','凶', '末吉']
+		// const results = ['大吉','大吉','大吉','大吉','凶']
+		// const n = Math.floor(Math.random()*results.length);
+		// btn.textContent = results[n];
+		// btn.textContent = results[Math.floor(Math.random()*results.length)];
+		const n = Math.random();
+		if (n < 0.05){
+			btn.textContent = '大吉';
+		}else if(n < 0.2){
+			btn.textContent = '中吉';
+		}else{
+			btn.textContent = '凶';
+		}
+		// switch (n){
+		// 	case 0:
+		// 	btn.textContent = '大吉'
+		// 	break;
+		// 	case 1:
+		// 	btn.textContent = '中吉'
+		// 	break;
+		// 	case 2:
+		// 	btn.textContent = '凶'
+		// 	break;
+		// }
+	});
+}
